e2cb36bcebafd6f46b52067b64a97a1586cd5a58
martins091 <agbomartinsejiofor@gmail.com>
2024-08-29 14:46:00 +0100

    update dbName

--- src/lib/mongodb/mongoose.js
+++ src/lib/mongodb/mongoose.js
@@ -12,7 +12,7 @@ export const connect = async () => {
 
     try {
         await mongoose.connect(process.env.MONGODB_URL, {
-            dbName:"next auth app",
+            dbName:"next-auth-app",
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
